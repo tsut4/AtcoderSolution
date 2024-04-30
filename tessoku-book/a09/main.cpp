e746e1cf39bb4cb11e1065eafb6774cf12dcf549
tsut4 <t4tsuy4n_fluffyty@outlook.jp>
2024-04-30 23:19:10 +0900

    to_b09

--- tessoku-book/a09/main.cpp
+++ tessoku-book/a09/main.cpp
@@ -8,17 +8,14 @@ int X[1509][1509], Z[1509][1509];
 int main() {
     // 入力
     cin >> H >> W >> N;
-    for (int t = 1; t <= N; t++) cin >> A[t] >> B[t] >> C[t] >> D[t];
+    for (int i = 1; i <= N; i++) cin >> A[i] >> B[i] >> C[i] >> D[i];
 
-    // 各日について加算
-    for(int t = 1; t <= N; t++) {
+    for (int t = 1; t <= N; t++) {
         X[A[t]][B[t]] += 1;
         X[A[t]][D[t] + 1] -= 1;
         X[C[t] + 1][B[t]] -= 1;
         X[C[t] + 1][D[t] + 1] += 1;
     }
-
-    // 二次元和をとる
     for (int i = 0; i <= H; i++) {
         for (int j = 0; j <= W; j++) Z[i][j] = 0;
     }
@@ -28,15 +25,13 @@ int main() {
     for (int j = 1; j <= W; j++) {
         for (int i = 1; i <= H; i++) Z[i][j] = Z[i - 1][j] + Z[i][j];
     }
-
     // 出力
     for (int i = 1; i <= H; i++) {
         for (int j = 1; j <= W; j++) {
-            if (j >= 2) cout << " ";
-            cout << Z[i][j];
+        if (j >= 2) cout << " ";
+        cout << Z[i][j];
         }
         cout << endl;
-
     }
     return 0;
 }
---
+++ tessoku-book/b09/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int N, T[1509][1509];
+int A[100009], B[100009], C[100009], D[100009];
+
+int main () {
+    // 入力
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> A[i] >> B[i] >> C[i] >>D[i];
+
+    // 各紙について +1/-1を加算
+    for (int i = 0; i <= 1500; i++) {
+        for (int j = 0; j <= 1500; j++) T[i][j] = 0;
+    }
+    for (int i = 1; i <= N; i++) {
+        T[A[i]][B[i]] += 1;
+        T[A[i]][D[i]] -= 1;
+        T[C[i]][B[i]] -= 1;
+        T[C[i]][D[i]] += 1;
+    }
+    // 二次元累積和をとる
+    for (int i = 0; i <= 1500; i++) {
+        for (int j = 1; j <= 1500; j++) T[i][j] = T[i][j - 1] + T[i][j];
+    }
+    for (int i = 1; i <= 1500; i++) {
+        for (int j = 0; j <= 1500; j++) T[i][j] = T[i - 1][j] + T[i][j];
+    }
+
+    //面積を答える
+    int Answer = 0;
+    for (int i = 0; i <= 1500; i++) {
+        for (int j = 0; j <= 1500; j++) {
+            if (T[i][j] >= 1) Answer += 1;
+        }
+    }
+    cout << Answer << endl;
+    return 0;
+}
